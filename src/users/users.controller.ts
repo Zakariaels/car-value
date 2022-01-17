aaea147507a0e25da7b9193ec36e114f2d59da23
zakaria <zakaria.elaissaoui.insa@gmail.com>
2022-01-17 22:09:50 +0100

    added create user functionality

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,13 @@
+import { UsersService } from './users.service';
 import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('auth')
 export class UsersController {
+    constructor(private usersService: UsersService) {}
 
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
-        console.log({
-            body
-        })
+        this.usersService.create(body.email, body.password);
     }
 }
