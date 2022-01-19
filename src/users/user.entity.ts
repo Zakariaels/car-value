61c7868d123bf2a085d85370a658d8206c4e65d8
zakaria <zakaria.elaissaoui.insa@gmail.com>
2022-01-19 23:02:36 +0100

    added serialization using interceptors

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,11 +1,27 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterRemove, AfterUpdate } from 'typeorm';
 
+const log = console.log
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
     id: number;
+
     @Column()
     email: string;
+
     @Column()
     password: string;
+
+    @AfterInsert()
+    logInsert() {
+        log('Inserted User with id = ' + this.id);
+    }
+    @AfterUpdate()
+    logUpdate() {
+        log('Updated User with id = ' + this.id);
+    }
+    @AfterRemove()
+    logRemove() {
+        log('Removed User with id = ' + this.id);
+    }
 }
